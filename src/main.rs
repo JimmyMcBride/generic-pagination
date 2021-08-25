099ca868f15fb545d13047b3dd321970e5e0e70d
JimmyMcBride <mcbride967@gmail.com>
2021-08-24 19:47:48 -0500

    fixed typo

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use paginatator::Paged;
 fn main() {
   let list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].iter();
   let current_page = 3;
-  let paginated_date = Paged::new(&list, 3);
-  let page = paginated_date.page(current_page).unwrap().0;
+  let paginated_data = Paged::new(&list, 3);
+  let page = paginated_data.page(current_page).unwrap().0;
   println!(
     "Page {} of {}: {:#?}",
-    current_page, paginated_date.total_pages, page
+    current_page, paginated_data.total_pages, page
   );
 }
